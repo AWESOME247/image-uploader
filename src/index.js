efb813c28155e116c5465ab5b73d83175d7e0bf6
AWESOME247 <omoye6996@gmail.com>
2023-03-20 13:59:17 +0100

    'done'

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ const app = new express.Router();
 const s3 = new AWS.S3({
   accessKeyId: process.env.AWS_ACCESS_KEY_ID,
   secretAccessKey: process.env.AWS_ACCESS_KEY,
-});
+})
 
 const bucketName = "red9ja-videos";
 
